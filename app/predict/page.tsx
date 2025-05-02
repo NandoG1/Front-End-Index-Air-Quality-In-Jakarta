8d760aebd108f40ff78a45c3794f796d0115b0bf
NandoG1 <fernandogunawan291105@gmail.com>
2025-05-02 23:26:35 +0700

    update api

--- app/predict/page.tsx
+++ app/predict/page.tsx
@@ -44,7 +44,7 @@ export default function PredictPage() {
   const [activeTab, setActiveTab] = useState("date")
   const [isPageLoaded, setIsPageLoaded] = useState(false)
 
-  const API_BASE_URL = "https://back-end-air-quality-index-in-jakarta-production.up.railway.app/api"
+  const API_BASE_URL = "https://back-end-air-quality-index-in-jakarta-production-d42f.up.railway.app/api"
 
   useEffect(() => {
     setIsPageLoaded(true)
